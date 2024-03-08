3298c87b289dfd728c4db71f44ec64a9b1972b60
Alok Dubey <alok.dubey22@spit.ac.in>
2024-03-08 17:02:43 +0530

    Leh or bta shi h ya nhi

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-console.log("lets write java script");
+document.addEventListener("DOMContentLoaded", function () {
+  const songList = document.getElementById("files");
+  fetchSongs();
 
-
-async function main() {
-  let a=  await fetch("http://127.0.0.1:5500/songs/");
-  let response= await a.text();
-  console.log(response);
-
-
+  async function fetchSongs() {
+    try {
+      const response = await fetch("http://127.0.0.1:5500/songs/");
+      if (!response.ok) {
+        throw new Error("Failed to fetch songs");
+      }
+      const html = await response.text();
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      const fileElements = doc.querySelectorAll("#files > li > a");
+      fileElements.forEach((fileElement) => {
+        const fileName = fileElement.querySelector(".name").textContent;
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<a href="/songs/${fileName}">${fileName}</a> `;
+        songList.appendChild(listItem);
+        console.log(listItem);
+      });
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
-  
-  main();
-  
-  
-  
+});
